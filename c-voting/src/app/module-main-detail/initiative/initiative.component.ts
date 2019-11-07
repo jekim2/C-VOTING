7f10356f5245767cc245fdaac2b747a602897483
mjkim2 <mjejeee@gmail.com>
2019-11-08 00:22:59 +0900

    initiative-detail(2)

--- c-voting/src/app/module-main-detail/initiative/initiative.component.ts
+++ c-voting/src/app/module-main-detail/initiative/initiative.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitiativeComponent implements OnInit {
 
+  //Flag
+  isRecommandlAlert = false;    // 추천 팝업 Flag
+
   constructor() { }
 
   ngOnInit() {
